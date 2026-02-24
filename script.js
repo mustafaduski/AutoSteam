5de83c60f4e4743089de2da4b11f945fc51a7dac
mustafaduski <1ksport76@gmail.com>
2026-02-25 01:30:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 function openPayment() {
     document.getElementById('paymentOverlay').style.display = 'flex';
 }
-
 function closePayment() {
     document.getElementById('paymentOverlay').style.display = 'none';
-}
-
-function copyWallet() {
-    const walletText = document.getElementById('walletAddr').innerText;
-    navigator.clipboard.writeText(walletText).then(() => {
-        alert("Wallet Address Copied!");
-    });
 }
