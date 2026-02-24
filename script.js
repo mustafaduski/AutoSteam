fdc7a89d25ab738e4ea375d66fdb8aae1cf83a1f
mustafaduski <1ksport76@gmail.com>
2026-02-24 19:08:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function payWithCrypto() {
-    const walletAddress = "ناونیشانی_وۆڵێتەکەت_لێرە_دابنێ";
-    const amount = "79.99 USDT (TRC20)";
-    
-    const message = `To complete your order, please send ${amount} to the following address:\n\n${walletAddress}\n\nAfter payment, send a screenshot to our support.`;
-    
-    alert(message);
-    // دەتوانیت لێرەدا کۆدێک دابنێیت کە ناونیشانەکە ئۆتۆماتیکی کۆپی بکات
-    navigator.clipboard.writeText(walletAddress);
+function showPaymentInfo() {
+    document.getElementById('paymentDetails').style.display = 'block';
+    document.getElementById('mainBuyBtn').style.display = 'none';
+}
+
+function copyWallet() {
+    const walletText = document.getElementById('walletAddr').innerText;
+    navigator.clipboard.writeText(walletText).then(() => {
+        alert("Address copied to clipboard!");
+    });
 }
